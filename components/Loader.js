1abec40befa277ce58b0382be1319dd817ce7010
Jon Monroe <berkona@gmail.com>
2019-04-23 22:26:40 -0700

    Better loader

--- components/Loader.js
+++ components/Loader.js
@@ -2,7 +2,13 @@ import React from 'react'
 import Loader from 'react-loaders'
 
 export default (props) => (
-  <div style={{ display: 'flex', justifyContent: 'center' }}>
-    <Loader type="ball-scale-ripple-multiple" />
+  <div style={{
+    display: 'flex',
+    marginTop: '200px',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%', }
+  }>
+    <Loader color="#8BA8BC" type="square-spin" />
   </div>
 )
