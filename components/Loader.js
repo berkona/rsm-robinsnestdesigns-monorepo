d2e2868f00d0df02ed2c7e215ed919db8650d8c6
Jon Monroe <berkona@gmail.com>
2019-04-23 20:39:15 -0700

    fix loader

--- components/Loader.js
+++ components/Loader.js
@@ -2,9 +2,7 @@ import React from 'react'
 import Loader from 'react-loaders'
 
 export default (props) => (
-  <div>
-    <p align="center">
-      <Loader type="ball-scale-ripple-multiple" />
-    </p>
+  <div style={{ display: 'flex', justifyContent: 'center' }}>
+    <Loader type="ball-scale-ripple-multiple" />
   </div>
 )
