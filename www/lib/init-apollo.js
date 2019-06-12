ca917b41735996350ef6a6f84b1fc616c7cb9dcd
Jon Monroe <berkona@gmail.com>
2019-06-12 06:11:35 -0700

    Fix some deployment issues

--- www/lib/init-apollo.js
+++ www/lib/init-apollo.js
@@ -1,3 +1,4 @@
+import url from 'url'
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { BatchHttpLink } from "apollo-link-batch-http";
@@ -11,13 +12,18 @@ if (!process.browser) {
   global.fetch = fetch
 }
 
-const httpLink = new BatchHttpLink({
-  uri: 'https://t0ia86s7y6.execute-api.us-east-1.amazonaws.com/dev/graphql'
-});
+const defaultValidHostRegex = /^[a-zA-Z0-9_\/\-\.]+.now\.sh$/
 
-function create (initialState) {
+function getAPIUrl(req) {
+  return process.browser ? '/graphql' : process.env.API_URL || 'http://localhost:3000/graphql'
+}
+
+function create (initialState, req) {
   // Check out https://github.com/zeit/next.js/pull/4611 if you want to use the AWSAppSyncClient
   const cache = new InMemoryCache().restore(initialState || {});
+  const httpLink = new BatchHttpLink({
+    uri: getAPIUrl(req)
+  })
   const client = new ApolloClient({
     connectToDevTools: process.browser,
     ssrMode: !process.browser, // Disables forceFetch on the server (so queries are only run once)
@@ -27,16 +33,16 @@ function create (initialState) {
   return client
 }
 
-export default function initApollo (initialState) {
+export default function initApollo (initialState, req) {
   // Make sure to create a new client for every server-side request so that data
   // isn't shared between connections (which would be bad)
   if (!process.browser) {
-    return create(initialState)
+    return create(initialState, req)
   }
 
   // Reuse client on the client-side
   if (!apolloClient) {
-    apolloClient = create(initialState)
+    apolloClient = create(initialState, req)
   }
 
   return apolloClient
