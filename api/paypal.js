5c61915970ce817a50d3069df6658cdb6b2bc46c
Jon Monroe <berkona@gmail.com>
2019-06-12 04:45:46 -0700

    Make not setting paypal_* env a blocking error

--- api/paypal.js
+++ api/paypal.js
@@ -1,9 +1,12 @@
 const paypal = require('@paypal/checkout-server-sdk');
 
 // Creating an environment
-let clientId =  process.env.PAYPAL_CLIENTID || 'sb'
-let clientSecret = process.env.PAYPAL_CLIENTSECRET || 'sb';
-let environment = new paypal.core.SandboxEnvironment(clientId, clientSecret);
-let client = new paypal.core.PayPalHttpClient(environment);
+if (!process.env.PAYPAL_CLIENTID || !process.env.PAYPAL_CLIENTSECRET)
+  throw new Error('You must set the environmental variables: PAYPAL_CLIENTID and PAYPAL_CLIENTSECRET before starting server')
+
+let clientId =  process.env.PAYPAL_CLIENTID
+let clientSecret = process.env.PAYPAL_CLIENTSECRET
+let environment = new paypal.core.SandboxEnvironment(clientId, clientSecret)
+let client = new paypal.core.PayPalHttpClient(environment)
 
 module.exports = client;
