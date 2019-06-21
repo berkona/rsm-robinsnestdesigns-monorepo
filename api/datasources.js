3292786321dd8e20deb1c071df20f55a28aa07df
Jon Monroe <berkona@gmail.com>
2019-06-20 21:12:52 -0700

    Add port

--- api/datasources.js
+++ api/datasources.js
@@ -1,18 +1,21 @@
 const { SQLDataSource } = require("datasource-sql")
 
-if (!process.env.SQL_ENGINE || !process.env.SQL_HOST || !process.env.SQL_USER || !process.env.SQL_PWD || !process.env.SQL_DB) {
-  throw new Error('You must set the environmental variables: SQL_ENGINE, SQL_HOST, SQL_USER, SQL_PWD, SQL_DB before starting server')
+if (!process.env.SQL_ENGINE || !process.env.SQL_PORT || !process.env.SQL_HOST || !process.env.SQL_USER || !process.env.SQL_PWD || !process.env.SQL_DB) {
+  throw new Error('You must set the environmental variables: SQL_ENGINE, SQL_PORT, SQL_HOST, SQL_USER, SQL_PWD, SQL_DB before starting server')
 }
 
-const knex = require('knex')({
-  client: process.env.SQL_ENGINE,
-  connection: {
-    host: process.env.SQL_HOST,
-    user: process.env.SQL_USER,
-    password: process.env.SQL_PWD,
-    database: process.env.SQL_DB,
-  },
-})
+if (typeof knex === 'undefined') {
+  var knex = require('knex')({
+    client: process.env.SQL_ENGINE,
+    connection: {
+      host: process.env.SQL_HOST,
+      user: process.env.SQL_USER,
+      port: process.env.SQL_PORT,
+      password: process.env.SQL_PWD,
+      database: process.env.SQL_DB,
+    },
+  })
+}
 
 const validateArgs = (args) => {
   args = Object.assign({}, { skip: 0, limit: 50, sort: 'relevancy' }, args)
