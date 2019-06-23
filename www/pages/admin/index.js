cf4872485b5bb0899a9807f8bbe05c264ae75c67
Jon Monroe <berkona@gmail.com>
2019-06-23 15:29:41 -0700

    Admin menu

---
+++ www/pages/admin/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Col from 'react-bootstrap/Col'
+import Link from 'next/link'
+
+export default (props) => (
+  <Col>
+    <h1>Admin Menu</h1>
+    <hr />
+    <ul>
+      <li>
+        <Link href="/admin/product-create">
+          <a>Create Product</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/admin/products">
+          <a>Modify Products</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/admin/category-create">
+          <a>Add Category</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/admin/categories">
+          <a>Modify Categories</a>
+        </Link>
+      </li>
+    </ul>
+  </Col>
+)
