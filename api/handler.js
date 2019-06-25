1a090fb4045a4606de02b03da762573325f75248
Jon Monroe <berkona@gmail.com>
2019-06-25 01:47:19 -0700

    Fixed

--- api/handler.js
+++ api/handler.js
@@ -6,21 +6,6 @@ const { verifyAuthToken, resolvers } = require('./resolvers')
 
 const isDev = process.env.NODE_ENV !== 'production'
 
-if (!process.env.SQL_ENGINE || !process.env.SQL_PORT || !process.env.SQL_HOST || !process.env.SQL_USER || !process.env.SQL_PWD || !process.env.SQL_DB) {
-  throw new Error('You must set the environmental variables: SQL_ENGINE, SQL_PORT, SQL_HOST, SQL_USER, SQL_PWD, SQL_DB before starting server')
-}
-
-const knex = require('knex')({
-  client: process.env.SQL_ENGINE,
-  connection: {
-    host: process.env.SQL_HOST,
-    user: process.env.SQL_USER,
-    port: process.env.SQL_PORT,
-    password: process.env.SQL_PWD,
-    database: process.env.SQL_DB,
-  },
-})
-
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -29,7 +14,7 @@ const server = new ApolloServer({
   tracing: isDev,
   dataSources: () => {
     return {
-      db: new MyDB(knex),
+      db: new MyDB(),
     }
   }
 })
