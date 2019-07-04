82b922b8c84c4668737a1ee8a45731ffcbbd4105
Jon Monroe <berkona@gmail.com>
2019-07-04 09:21:38 -0700

    Deploy bump

--- www/constants/config.js
+++ www/constants/config.js
@@ -1,3 +1,4 @@
+
 export const BASE_URL = process.browser ? window.location.origin : process.env.SITE_URL || 'http://localhost:3000/'
 export const PAYPAL_CLIENTID_LIVE = "AXWkXzWlmvbQYFl6Y3UxYRUjycNIR7BePYxItiZlKaYLJLGg6vOCffSmnw70QHGxuyfpte9yyH09GaZS"
 export const NO_IMAGE_URL = "https://public-images-static-af327.robinsnestdesigns.com/no-image.png"
